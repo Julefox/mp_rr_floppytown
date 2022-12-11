384d81c9c97ed8ee1cae01eea9461e68db8569b9
Julefox_ <Julien.aub13@gmail.com>
2022-12-11 18:57:18 +0100

    add .bat

---
+++ renameVPK.bat
@@ -0,0 +1,6 @@
+
+del "F:\Github\mp_rr_floppytown\packedVPK\client_mp_rr_floppytown.bsp.pak000_000.vpk"
+del "F:\Github\mp_rr_floppytown\packedVPK\englishclient_mp_rr_floppytown.bsp.pak000_dir.vpk"
+
+ren "F:\Github\mp_rr_floppytown\packedVPK\pak000_000.vpk" "client_mp_rr_floppytown.bsp.pak000_000.vpk"
+ren "F:\Github\mp_rr_floppytown\packedVPK\pak000_dir.vpk" "englishclient_mp_rr_floppytown.bsp.pak000_dir.vpk"
---
+++ repakRPAK.bat
@@ -0,0 +1,2 @@
+
+"F:\Github\mp_rr_floppytown\RePak and Tools\RePak\RePak.exe" "F:\Github\mp_rr_floppytown\RePak and Tools\RePak\mp_rr_floppytown.json"
